4954d825cbe64a97a41d4073222fc2e394544042
Vaishnavi Nagrale <Vaishnavinagrale2@gmail.com>
2024-06-19 19:23:38 +0530

    some changes done

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import './App.css'
 import Home from './components/Home'
 import Navbar from './components/Navbar'
 import VideoBackground from './components/VideoBackground'
-import bg from './assets/bg3.mp4'
 
 function App() {
 
